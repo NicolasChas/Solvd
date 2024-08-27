1d420bd09ed2420d9e8d39a24efbf1737e4630c0
NicolasChas <54710485+NicolasChas@users.noreply.github.com>
2024-08-27 00:10:34 -0300

    Update Class_Hierarchy.java
    Added exceptions, packages for everything, could not make logger work yet.

--- Class_Hierarchy.java
+++ Class_Hierarchy.java
@@ -2,298 +2,19 @@
 //Override of hashCode is in HighschoolStudent class.
 //Override of equals is in Person class.
 //Static block is in Worker class.
-abstract class Person{
-    private int id;
-    String name;
-    int age;
-    static int year=2024;
-    
-    public Person(int id,String name, int age){
-        this.id=id;
-        this.name=name;
-        this.age=age;
-    }
+//Try catch with resources is at the end of the program
+import Classes.*;
+import Classes.Exceptions.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-    public int getId(){
-        return id;
-    }
-    
-    public void setId(int id){
-        this.id=id;
-    }
-
-    static void printYear(){
-        System.out.println("The year is "+year);
-    }
-    
-
-    public abstract void printName();
-//Equals override to return true if two persons have the same id.
-    @Override
-    public boolean equals(Object person){
-        if(this.id==((Person) person).getId()){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-}
-
-interface wakeUp{
-    public void wakeUpTime();
-}
-
-interface sleep{
-    public void sleepTime();
-}
-
-interface play{
-    public void playTime();
-}
-
-interface eat{
-    public void eatingTime();
-}
-
-interface dream{
-    public void dreamTime();
-}
-
-
-class Student extends Person{
-    String grade;
-    public Student(int id,String name, int age, String grade){
-        super(id,name,age);
-        this.grade=grade;
-    }
-//Override of abstract method
-    @Override
-    public void printName() {
-        System.out.println("The Student's name is "+ this.name);
-    }
-    
-}
-
-    class HighschoolStudent extends Student implements wakeUp{
-        protected boolean privateSchool;
-        public HighschoolStudent(int id,String name,int age,String grade,boolean privateSchool){
-            super(id, name, age, grade);
-            this.privateSchool=privateSchool;
-        }
-//Override of hashcode, adds the person's id to its age to create a code.
-        @Override
-        public int hashCode(){
-            int code=this.getId() + this.age;
-            return code;
-        }
-    @Override
-        public void wakeUpTime(){
-            System.out.println("This Highschool Student wakes up at 09:00 AM");
-        }
-    }
-
-    class ElementaryStudent extends Student implements play{
-        protected String favColor;
-        public ElementaryStudent(int id,String name,int age, String grade,String favColor){
-            super(id, name, age, grade);
-            this.favColor=favColor;
-        }
-        @Override
-        public void playTime() {
-            System.out.println("This Elementary Student plays with toys");
-        }
-    }
-
-    class CollegeStudent extends Student{
-        protected String career;
-        public CollegeStudent(int id, String name, int age, String grade,String career){
-            super(id, name, age, grade);
-            this.career=career;
-        }
-    }
-
-class Worker extends Person{
-    private String cardNumber; 
-    public Worker(int id,String name, int age,String cardNumber){
-        super(id,name,age);
-        this.cardNumber=cardNumber;
-    }
-
-    public String getCardNumber(){
-        return cardNumber;
-    }
-
-    public void setCardNumber(String cardNumber){
-        this.cardNumber=cardNumber;
-    }
-    @Override
-    public void printName(){
-        System.out.println("The Worker's name is "+ this.name);
-    }   
-    static {
-        System.out.println("All workers must pay taxes before the end of the month.");
-    }
-    final void payTaxes(){
-        System.out.println("You have paid your taxes");
-    }
-
-}
-
-    class Teacher extends Worker implements sleep{
-        String field;
-        public Teacher(int id,String name, int age,String cardNumber,String field){
-            super(id,name,age,cardNumber);
-            this.field=field;
-        }
-        @Override
-        public String toString(){
-            return "This teacher's name is "+name+ ", and their field is "+field+".";
-        }
-        @Override
-        public void sleepTime() {
-            System.out.println("This teacher sleeps at 01:00 AM");
-        }
-    }
-
-    class Banker extends Worker{
-        protected String bank;
-        private String password;
-        public Banker(int id,String name, int age,String cardNumber,String bank, String password){
-            super(id,name,age,cardNumber);
-            this.bank=bank;
-            this.password=password;
-        }
-
-        public String getPassword(){
-            return password;
-        }
-
-        public void setPassword(String password){
-            this.password=password;
-        }
-    }
-
-    class Politician extends Worker{
-        protected String position;
-        private int salary;
-        public Politician(int id,String name, int age,String cardNumber,String position, int salary){
-            super(id,name,age,cardNumber);
-            this.position=position;
-            this.salary=salary;
-        }
-
-        public int getSalary(){
-            return salary;
-        }
-
-        public void setSalary(int salary){
-            this.salary=salary;
-        }
-//Override of method/Polymorphism of abstrac
-        @Override
-        public void setId(int id){
-            super.setId(id);
-            System.out.println("You have set the id to: "+id);
-        }
-    
-    }
-
-    class Farmer extends Worker implements eat{
-        String farmType;
-        public Farmer(int id,String name, int age,String cardNumber,String farmType){
-            super(id,name,age,cardNumber);
-            this.farmType=farmType;
-        }
-        @Override
-        public void eatingTime() {
-            System.out.println("This farmer eats eggs and chicken.");
-        } 
-    }
-
-    class Lawyer extends Worker{
-        protected int casesWon;
-        private int casesLost;
-        final boolean inDebt=true;
-        public Lawyer(int id,String name, int age,String cardNumber,int casesWon,int casesLost){
-            super(id,name,age,cardNumber);
-            this.casesWon=casesWon;
-            this.casesLost=casesLost;
-        } 
-
-        public int getCasesLost(){
-            return casesLost;
-        }
-
-        public void setCasesLost(int casesLost){
-            this.casesLost=casesLost;
-        }
-    }
-
-    class Chef extends Worker{
-        String specialization;
-        private String secretRecipe;
-        public Chef(int id,String name, int age,String cardNumber,String specialization,String recipe){
-            super(id,name,age,cardNumber);
-            this.specialization=specialization;
-            this.secretRecipe=recipe;
-        }
-        public String getSecretRecipe(){
-            return secretRecipe;
-        }
-
-        public void setSecretRecipe(String secretRecipe){
-            this.secretRecipe=secretRecipe;
-        }
-    }
-    
-    class Athlete extends Worker implements dream{
-        String sport;
-        protected boolean fit;
-        public Athlete(int id,String name, int age,String cardNumber,String sport,boolean fit){
-            super(id,name,age,cardNumber);
-            this.sport=sport;
-            this.fit=fit;
-        } 
-    //Overloading constructor
-        public Athlete(int id,String name,int age,String cardNumber,String sport){
-            super(id, name, age,cardNumber);
-            this.sport=sport;
-            this.fit=true;
-        }
-        @Override
-        public void dreamTime() {
-            System.out.println("This athlete dreams of winning a gold medal.");
-        }
-    }
-
-    class Plumber extends Worker{
-        private boolean plumbingLicense;
-        public Plumber(int id,String name, int age,String cardNumber,boolean plumbingLicense){
-            super(id,name,age,cardNumber);
-            this.plumbingLicense=plumbingLicense;
-        } 
-
-        public boolean getLicense(){
-            return plumbingLicense;
-        }
-
-        public void setLicense(boolean plumbingLicense){
-            this.plumbingLicense=plumbingLicense;
-        }
-    }
-
-    final class unemployed extends Worker{
-        public unemployed(int id,String name, int age, String cardNumber){
-            super(id,name,age,cardNumber);
-        }
-    }
 
 public class Class_Hierarchy {
     public static void main(String[] args) {
         HighschoolStudent highschoolStudent= new HighschoolStudent(4321, "Peter Cerk", 15, "Second Year", false);
         ElementaryStudent elementaryStudent= new ElementaryStudent(1574, "Thomas Plank", 10, "Sixth grade", "Blue");
-        CollegeStudent collegeStudent= new CollegeStudent(4831, "Carter Jones", 20, "Sophomore", "Engineering");
+        CollegeStudent collegeStudent=new CollegeStudent(3217, "Carter Jones", 21, "Sophomore", "Engineering", true);
         Teacher teacher= new Teacher(6913, "Helena Pierce", 32,"44459901", "Math");
         Banker banker= new Banker(8710, "Kevin Doyle", 37, "86470912", "Central Bank", "apples00");
         Politician politician= new Politician(3371, "Tim Becker", 55, "18430091", "Senator", 500000);
@@ -307,6 +28,99 @@ public class Class_Hierarchy {
         System.out.println(farmer.equals(plumber));
         highschoolStudent.wakeUpTime();
         farmer.printYear();
+        try{
+            checkTitle(collegeStudent.highSchoolTitle);
+        } catch(InvalidTitle e){
+            System.out.println(e);
+        }
+        try{
+            checkElementaryStudentAge(elementaryStudent.age);
+        } catch(InvalidAge e){
+            System.out.println(e);
+        }
+        try{
+            checkPasswordSecurity(banker.getPassword());
+        } catch(InvalidPassword e){
+            System.out.println(e);
+        }
+        try{
+            checkSalary(politician.getSalary());
+        } catch(InvalidSalary e){
+            System.out.println(e);
+        }
+        try{
+            checkFit(athlete.getFit());
+        } catch(InvalidFit e){
+            System.out.println(e);
+        }
+        try{
+            readFirstLineFromFile("file.txt");
+        }catch(IOException e){
+            System.out.println(e);
+        }
+        checkPlumberAge(plumber.age);
+
     }
-    
+    static void checkTitle(boolean title) throws InvalidTitle{
+        if (title!=true){
+            throw new InvalidTitle("You cant create a college student without a highschool title.");
+        }
+        else {
+            System.out.println("No problems with your College student's credentials.");
+        }
+    }
+
+    static void checkElementaryStudentAge(int age) throws InvalidAge{
+        if (age>11){
+            throw new InvalidAge("You cant create an elementary student who is older than 11.");
+        }
+        else {
+            System.out.println("No problems with your Elementary student's credentials.");
+        }
+    }
+
+    static void checkPasswordSecurity(String password) throws InvalidPassword{
+        if (password.length()<=4){
+            throw new InvalidPassword("Banker's password must be more than 4 characters.");
+        }
+        else{
+            System.out.println("No problems with your Banker's credentials.");
+        }
+    }
+
+    static void checkSalary(int salary) throws InvalidSalary{
+        if(salary<100000){
+            throw new InvalidSalary("Salary of politician must be higher than 6 figures.");
+        }
+        else{
+            System.out.println("No problems with your politician's credentials.");
+        } 
+    }
+
+    static void checkFit(boolean fit) throws InvalidFit{
+        if(fit!=true){
+            throw new InvalidFit("Athlete must be fit");
+        }
+        else{
+            System.out.println("No problems with your Athlete's credentials.");
+        }
+    }
+
+    static void checkPlumberAge(int age)throws IncorrectPlumberAge{
+        if(age<40){
+            throw new IncorrectPlumberAge("The plumber's age should be above 40, because they have more experience");
+        }
+        else{
+            System.out.println("The plumber's age is acceptable");
+        }
+    }
+//try catch with resources
+    static String readFirstLineFromFile(String path) throws IOException {
+	    try (FileReader fr = new FileReader(path);
+	         BufferedReader br = new BufferedReader(fr)) {
+	        return br.readLine();
+	    }
+	}	
 }
+
+
